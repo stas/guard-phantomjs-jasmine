d9b34a106f74d93a939b218628212889929b0df0
Stas SUȘCOV <stas@net.utcluj.ro>
2012-09-13 17:41:50 +0300

    Move cli part into a separate file so we can use it in a rake task.

--- lib/guard/phantomjs-jasmine.rb
+++ lib/guard/phantomjs-jasmine.rb
@@ -1,9 +1,9 @@
-require 'guard'
 require 'guard/guard'
-require 'json'
+require 'guard/phantomjs-jasmine/cli'
 
 module Guard
   class PhantomJsJasmine < Guard
+    include PhantomJsJasmineCli
 
     # Initialize Guard::PhantomJsJasmine
     #
@@ -14,9 +14,6 @@ module Guard
     def initialize(watchers = [], options = {})
       super(watchers, options)
 
-      runner_script = 'phantomjs-jasmine/js/jasmine_runner.coffee'
-      options[:runner_script] ||= File.expand_path(runner_script, File.dirname(__FILE__))
-      options[:runner] ||= File.expand_path('spec/runner.html')
       @options = options
     end
 
@@ -31,44 +28,27 @@ module Guard
     def run_on_change(paths)
       return if paths.empty?
 
-      cmd = "phantomjs #{@options[:runner_script]} #{@options[:runner]}"
-
-      result = IO.popen(cmd)
-      parsed_result = JSON.parse(result.read, {
-        :max_nesting => false, :symbolize_names => true })
-      result.close
-
-      passed = parsed_result[:passed]
-      failed = parsed_result[:total] - passed
-      run_time = (parsed_result[:ended] - parsed_result[:started]).round(3)
-
-      if failed > 0
-        notify("#{failed} Jasmine spec(s) failed.", :failed)
-        parsed_result[:suites].each do |suite|
-          error("\n#{suite[:name]}\n\t#{suite[:spec]}\n\t\t#{suite[:trace][:message]}") unless suite[:passed]
-        end
-
-        info("\n#{failed}/#{parsed_result[:total]} failed in #{run_time} ms.")
-      else
-        notify("Jasmine specs passed.", :success)
-        info("\n#{parsed_result[:total]} passed in #{run_time} ms.")
-      end
+      run(@options)
     end
 
     private
 
     # Wrapper against Guard::Notifier#notify
+    # @param [String] message, the text to output
+    # @param [Symbol] image, the text image
     def notify(message, image)
       ::Guard::Notifier.notify(
         message, :title => 'PhantomJS Jasmine Guard', :image => image)
     end
 
     # Wrapper against Guard::UI#error
+    # @param [String] message, the text to output
     def error(message)
       ::Guard::UI.error(message)
     end
 
     # Wrapper against Guard::UI#info
+    # @param [String] message, the text to output
     def info(message)
       ::Guard::UI.info(message)
     end
---
+++ lib/guard/phantomjs-jasmine/cli.rb
@@ -0,0 +1,70 @@
+require 'json'
+
+module Guard
+  # PhantomJS Jasmine command line utils
+  #
+  module PhantomJsJasmineCli
+
+    # Command line runner
+    # Separated from guard so it's possible to be used in rake tasks too
+    # @param [Hash] options, to be passed to phantomjs
+    #
+    def run(options)
+      options[:runner] ||= 'spec/runner.html'
+      options[:runner_script] ||= File.expand_path(
+        'js/jasmine_runner.coffee',
+        File.dirname(__FILE__))
+
+      cmd = "phantomjs #{options[:runner_script]} #{options[:runner]}"
+      result = IO.popen(cmd)
+
+      parsed_result = JSON.parse(result.read, {
+        :max_nesting => false, :symbolize_names => true })
+      result.close
+
+      passed = parsed_result[:passed]
+      failed = parsed_result[:total] - passed
+      run_time = (parsed_result[:ended] - parsed_result[:started]).round(3)
+
+      if failed > 0
+        notify("#{failed} Jasmine spec(s) failed.", :failed)
+        parsed_result[:suites].each do |suite|
+          if suite[:passed]
+            info(
+              "\n#{suite[:name]}\n\t#{suite[:spec]}"
+            )
+          else
+            error(
+              "\n#{suite[:name]}\n\t#{suite[:spec]}\n\t\t#{suite[:trace][:message]}"
+            )
+          end
+        end
+
+        info("\n#{failed}/#{parsed_result[:total]} failed in #{run_time} ms.")
+      else
+        notify("Jasmine specs passed.", :success)
+        info("\n#{parsed_result[:total]} passed in #{run_time} ms.")
+      end
+    end
+
+    # Helper to output info messages
+    # @param [String] msg, the text to output
+    def info(msg)
+      puts msg
+    end
+
+    # Helper to output error messages, colors in red
+    # @param [String] msg, the text to output
+    def error(msg)
+      puts "\e[0;31m#{ msg }\e[0m"
+    end
+
+    # Helper to send notifications, not implemented
+    # We don't use notify in CLI sessions
+    # @param [String] msg, the text to output
+    # @param [Symbol] image, the text image
+    def notify(msg, image)
+    end
+
+  end
+end
